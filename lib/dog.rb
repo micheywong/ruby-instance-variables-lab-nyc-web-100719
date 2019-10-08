518cb98639d8279edca6528fcc3fa38857677148
Michelle Wong <michellekwong2@gmail.com>
2019-10-08 15:23:32 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,6 +1,6 @@
 class Dog
   def name=(dog_name)
-    this_dogs_name = dog_name
+    @this_dogs_name = dog_name
   end
  
   def name
@@ -8,7 +8,7 @@ class Dog
   end
   
   lassie = Dog.new
-lassie.name = "Lassie"
+  lassie.name = "Lassie"
  
-lassie.name
+  lassie.name
 end
